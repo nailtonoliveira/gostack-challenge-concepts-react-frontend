73901644f3471d0ab2e59f6d8c06f8a6d81d7bee
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2020-06-18 01:05:27 -0400

    implementation of get list from api and show her on page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function getRepositories() {
+      const response = await api.get('repositories');
+      
+      setRepositories(response.data);
+    }
+
+    getRepositories();
+  }, [])
+
   async function handleAddRepository() {
     // TODO
   }
@@ -14,13 +29,15 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(1)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
